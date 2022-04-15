7de7f863bbd53f6c06a27c80a0558a7f5e2928e3
vitaxa <vitaxa93gamebox@gmail.com>
2022-04-15 13:26:37 +0300

    TD-267: migrate from deprecated enum bank card

--- src/main/java/dev/vality/hooker/utils/PaymentToolUtils.java
+++ src/main/java/dev/vality/hooker/utils/PaymentToolUtils.java
@@ -3,14 +3,15 @@ package dev.vality.hooker.utils;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import dev.vality.damsel.domain.BankCard;
-import dev.vality.damsel.domain.LegacyDigitalWalletProvider;
 import dev.vality.damsel.domain.MobilePhone;
+import dev.vality.damsel.domain.PaymentSystemRef;
 import dev.vality.damsel.domain.PaymentTool;
-import dev.vality.mamsel.*;
 import dev.vality.swag_webhook_events.model.*;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.Base64;
+import java.util.Optional;
 
 public class PaymentToolUtils {
 
@@ -18,30 +19,45 @@ public class PaymentToolUtils {
 
     public static PaymentToolDetails getPaymentToolDetails(PaymentTool paymentTool) {
         if (paymentTool.isSetBankCard()) {
+            PaymentToolDetailsBankCard.TokenProviderEnum tokenProvider =
+                    Arrays.stream(PaymentToolDetailsBankCard.TokenProviderEnum.values())
+                            .filter(tokenProviderEnum ->
+                                    tokenProviderEnum.name().equalsIgnoreCase(
+                                            paymentTool.getBankCard().getPaymentToken().getId()
+                                    )
+                            )
+                            .findFirst().orElse(PaymentToolDetailsBankCard.TokenProviderEnum.UNKNOWN);
             return new PaymentToolDetailsBankCard()
                     .bin(paymentTool.getBankCard().getBin())
                     .lastDigits(paymentTool.getBankCard().getLastDigits())
                     .cardNumberMask(
                             paymentTool.getBankCard().getBin() + "******" + paymentTool.getBankCard().getLastDigits())
-                    .tokenProvider(paymentTool.getBankCard().getTokenProviderDeprecated() != null
-                            ? PaymentToolDetailsBankCard.TokenProviderEnum.fromValue(
-                            paymentTool.getBankCard().getTokenProviderDeprecated().name())
+                    .tokenProvider(paymentTool.getBankCard().getPaymentToken() != null
+                            ? tokenProvider
                             : null)
-                    .paymentSystem(PaymentSystemUtil.getPaymentSystemName(paymentTool.getBankCard()))
+                    .tokenProviderName(paymentTool.getBankCard().getPaymentToken().getId())
+                    .paymentSystem(
+                            Optional.ofNullable(paymentTool.getBankCard().getPaymentSystem())
+                                    .map(PaymentSystemRef::getId).orElse(null))
                     .issuerCountry(paymentTool.getBankCard().getIssuerCountry() != null
                             ? paymentTool.getBankCard().getIssuerCountry().name()
                             : null)
                     .bankName(paymentTool.getBankCard().getBankName())
                     .detailsType(PaymentToolDetails.DetailsTypeEnum.PAYMENTTOOLDETAILSBANKCARD);
         } else if (paymentTool.isSetPaymentTerminal()) {
+            PaymentToolDetailsPaymentTerminal.ProviderEnum provider =
+                    Arrays.stream(PaymentToolDetailsPaymentTerminal.ProviderEnum.values())
+                            .filter(providerEnum ->
+                                    providerEnum.name().equalsIgnoreCase(
+                                            paymentTool.getPaymentTerminal().getPaymentService().getId())
+                            )
+                            .findFirst().orElse(PaymentToolDetailsPaymentTerminal.ProviderEnum.UNKNOWN);
             return new PaymentToolDetailsPaymentTerminal()
-                    .provider(PaymentToolDetailsPaymentTerminal.ProviderEnum.fromValue(
-                            TerminalPaymentUtil.getTerminalPaymentProviderName(paymentTool.getPaymentTerminal())))
+                    .provider(provider)
+                    .providerName(paymentTool.getPaymentTerminal().getPaymentService().getId())
                     .detailsType(PaymentToolDetails.DetailsTypeEnum.PAYMENTTOOLDETAILSPAYMENTTERMINAL);
         } else if (paymentTool.isSetDigitalWallet()) {
-            LegacyDigitalWalletProvider walletProvider = LegacyDigitalWalletProvider.valueOf(
-                    DigitalWalletUtil.getDigitalWalletName(paymentTool.getDigitalWallet()));
-            if (walletProvider == LegacyDigitalWalletProvider.qiwi) {
+            if (paymentTool.getDigitalWallet().getPaymentService().getId().equals("qiwi")) {
                 return new PaymentToolDetailsDigitalWallet()
                         .digitalWalletDetailsType(
                                 PaymentToolDetailsDigitalWallet.DigitalWalletDetailsTypeEnum.DIGITALWALLETDETAILSQIWI)
@@ -49,9 +65,13 @@ public class PaymentToolUtils {
             } else {
                 throw new UnsupportedOperationException("Unknown digital wallet type");
             }
-        } else if (CryptoCurrencyUtil.isSetCryptoCurrency(paymentTool)) {
+        } else if (paymentTool.isSetCryptoCurrency()) {
+            CryptoCurrency cryptoCurrency = Arrays.stream(CryptoCurrency.values())
+                    .filter(crypto -> crypto.name().equalsIgnoreCase(paymentTool.getCryptoCurrency().getId()))
+                    .findFirst().orElse(CryptoCurrency.UNKNOWN);
             return new PaymentToolDetailsCryptoWallet()
-                    .cryptoCurrency(CryptoCurrency.fromValue(CryptoCurrencyUtil.getCryptoCurrencyName(paymentTool)))
+                    .cryptoCurrency(cryptoCurrency)
+                    .cryptoCurrencyType(paymentTool.getCryptoCurrency().getId())
                     .detailsType(PaymentToolDetails.DetailsTypeEnum.PAYMENTTOOLDETAILSCRYPTOWALLET);
         } else if (paymentTool.isSetMobileCommerce()) {
             return new PaymentToolDetailsMobileCommerce()
@@ -70,23 +90,23 @@ public class PaymentToolUtils {
             BankCard paymentCard = paymentTool.getBankCard();
             rootNode.put("type", "bank_card");
             rootNode.put("token", paymentCard.getToken());
-            rootNode.put("payment_system", PaymentSystemUtil.getPaymentSystemName(paymentCard));
+            rootNode.put("payment_system", paymentCard.getPaymentSystem().getId());
             rootNode.put("bin", paymentCard.getBin());
             rootNode.put("masked_pan", paymentCard.getLastDigits());
-            rootNode.put("token_provider", TokenProviderUtil.getTokenProviderName(paymentCard));
+            rootNode.put("token_provider", paymentCard.getPaymentToken().getId());
         } else if (paymentTool.isSetPaymentTerminal()) {
             rootNode.put("type", "payment_terminal");
             rootNode.put(
                     "terminal_type",
-                    TerminalPaymentUtil.getTerminalPaymentProviderName(paymentTool.getPaymentTerminal())
+                    paymentTool.getPaymentTerminal().getPaymentService().getId()
             );
         } else if (paymentTool.isSetDigitalWallet()) {
             rootNode.put("type", "digital_wallet");
-            rootNode.put("provider", DigitalWalletUtil.getDigitalWalletName(paymentTool.getDigitalWallet()));
+            rootNode.put("provider", paymentTool.getDigitalWallet().getPaymentService().getId());
             rootNode.put("id", paymentTool.getDigitalWallet().getId());
-        } else if (CryptoCurrencyUtil.isSetCryptoCurrency(paymentTool)) {
+        } else if (paymentTool.isSetCryptoCurrency()) {
             rootNode.put("type", "crypto_currency");
-            rootNode.put("crypto_currency", CryptoCurrencyUtil.getCryptoCurrencyName(paymentTool));
+            rootNode.put("crypto_currency", paymentTool.getCryptoCurrency().getId());
         } else if (paymentTool.isSetMobileCommerce()) {
             rootNode.put("type", "mobile_commerce");
             MobilePhone mobilePhone = paymentTool.getMobileCommerce().getPhone();
