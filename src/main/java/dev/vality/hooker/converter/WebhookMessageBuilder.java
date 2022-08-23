ac3f1fc6c8b4ee5599c65ec4a205434a98af9fe3
Inal Arsanukaev <aiz@empayre.com>
2022-08-23 19:23:23 +0300

    Fill parent id

--- src/main/java/dev/vality/hooker/converter/WebhookMessageBuilder.java
+++ src/main/java/dev/vality/hooker/converter/WebhookMessageBuilder.java
@@ -31,7 +31,7 @@ public class WebhookMessageBuilder {
                 .setWebhookId(webhookMessageModel.getHookId())
                 .setSourceId(sourceId)
                 .setEventId(event.getEventID())
-                .setParentEventId(-1) //TODO REMOVE
+                .setParentEventId(parentId)
                 .setCreatedAt(TypeUtil.temporalToString(Instant.now()))
                 .setUrl(webhookMessageModel.getUrl())
                 .setContentType(ContentType.APPLICATION_JSON.getMimeType())
