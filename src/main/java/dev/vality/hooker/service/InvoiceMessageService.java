8c55df48fcf9dda5afd85ff42559d92f855041eb
Kostya Struga <kennny007@yandex.ru>
2024-03-27 17:22:48 +0300

    Change logic

--- src/main/java/dev/vality/hooker/service/InvoiceMessageService.java
+++ src/main/java/dev/vality/hooker/service/InvoiceMessageService.java
@@ -2,7 +2,6 @@ package dev.vality.hooker.service;
 
 import dev.vality.hooker.converter.WebhookMessageBuilder;
 import dev.vality.hooker.dao.InvoicingMessageDao;
-import dev.vality.hooker.dao.MessageDao;
 import dev.vality.hooker.model.InvoicingMessage;
 import dev.vality.swag_webhook_events.model.Event;
 import dev.vality.webhook.dispatcher.WebhookMessage;
@@ -19,26 +18,27 @@ public class InvoiceMessageService {
 
     public void process(InvoicingMessage message) {
         log.info("Start processing of message {}", message);
-        if (messageDao.hasWebhooks(message)) {
-            Long id = messageDao.save(message);
-            String sourceId = message.getSourceId();
-            if (id != null) {
-                message.setId(id);
-                var webhookModels = messageDao.getWebhookModels(id);
-                if (!webhookModels.isEmpty()) {
-                    log.info("Processing {} webhook(s)", webhookModels.size());
-                    Event event = eventService.getEventByMessage(message);
-                    webhookModels.forEach(w -> {
-                        Long hookId = w.getHookId();
-                        Long parentEventId = messageDao.getParentId(hookId, sourceId, id);
-                        WebhookMessage webhookMessage = webhookMessageBuilder.build(w, event, sourceId, parentEventId);
-                        log.info("Try to send webhook to kafka: {}, parentId {}", webhookMessage, parentEventId);
-                        webhookKafkaProducerService.send(webhookMessage);
-                        log.info("Webhook to kafka was sent: sourceId={}", webhookMessage.getSourceId());
-                    });
-                }
-            }
-            log.info("End processing of message {}", sourceId);
+        if (!messageDao.hasWebhooks(message)) {
+            log.info("End without hook processing of message {}", message);
         }
+        Long id = messageDao.save(message);
+        String sourceId = message.getSourceId();
+        if (id != null) {
+            message.setId(id);
+            var webhookModels = messageDao.getWebhookModels(id);
+            if (!webhookModels.isEmpty()) {
+                log.info("Processing {} webhook(s)", webhookModels.size());
+                Event event = eventService.getEventByMessage(message);
+                webhookModels.forEach(w -> {
+                    Long hookId = w.getHookId();
+                    Long parentEventId = messageDao.getParentId(hookId, sourceId, id);
+                    WebhookMessage webhookMessage = webhookMessageBuilder.build(w, event, sourceId, parentEventId);
+                    log.info("Try to send webhook to kafka: {}, parentId {}", webhookMessage, parentEventId);
+                    webhookKafkaProducerService.send(webhookMessage);
+                    log.info("Webhook to kafka was sent: sourceId={}", webhookMessage.getSourceId());
+                });
+            }
+        }
+        log.info("End processing of message {}", sourceId);
     }
 }
