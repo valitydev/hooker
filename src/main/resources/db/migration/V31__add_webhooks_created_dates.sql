68732ce4953e4124ba694f64cd35eeda86127cf8
kostyastruga <koztyastruga@gmail.com>
2024-09-19 15:33:20 +0300

    Add time check

--- src/main/resources/db/migration/V31__add_webhooks_created_dates.sql
+++ src/main/resources/db/migration/V31__add_webhooks_created_dates.sql
@@ -1,3 +1,2 @@
-ALTER TABLE hook.webhook ADD COLUMN created_at TIMESTAMP DEFAULT NOW()
-
-ALTER TABLE hook.webhook_to_events ADD COLUMN created_at TIMESTAMP DEFAULT NOW()
+ALTER TABLE hook.webhook ADD COLUMN created_at TIMESTAMP DEFAULT NOW();
+ALTER TABLE hook.webhook_to_events ADD COLUMN created_at TIMESTAMP DEFAULT NOW();
