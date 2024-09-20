68f789c3bc64c241cf30a94e5414a74a8a6bf44e
kostyastruga <koztyastruga@gmail.com>
2024-09-20 15:19:07 +0300

    Add time check

--- src/main/resources/db/migration/V31__add_webhooks_created_dates.sql
+++ src/main/resources/db/migration/V31__add_webhooks_created_dates.sql
@@ -1,9 +1,9 @@
 ALTER TABLE hook.webhook
-    ADD COLUMN IF NOT EXISTS created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
-UPDATE hook.webhook
-SET created_at = NOW() - INTERVAL '2 DAYS';
+    ADD COLUMN IF NOT EXISTS created_at TIMESTAMP NOT NULL DEFAULT clock_timestamp();
+-- UPDATE hook.webhook
+-- SET created_at = NOW() - INTERVAL '2 DAYS';
 
 ALTER TABLE hook.webhook_to_events
-    ADD COLUMN IF NOT EXISTS created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
-UPDATE hook.webhook_to_events
-SET created_at = NOW() - INTERVAL '2 DAYS';
+    ADD COLUMN IF NOT EXISTS created_at TIMESTAMP NOT NULL DEFAULT clock_timestamp();
+-- UPDATE hook.webhook_to_events
+-- SET created_at = NOW() - INTERVAL '2 DAYS';
