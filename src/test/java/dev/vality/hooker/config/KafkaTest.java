c13252f719a00445d4c75feec445856d0495076a
vitaxa <vitaxa93gamebox@gmail.com>
2025-11-14 18:09:50 +0300

    fix test kafka image [5]

--- src/test/java/dev/vality/hooker/config/KafkaTest.java
+++ src/test/java/dev/vality/hooker/config/KafkaTest.java
@@ -1,19 +1,20 @@
 package dev.vality.hooker.config;
 
-
-import dev.vality.testcontainers.annotations.KafkaConfig;
-import dev.vality.testcontainers.annotations.kafka.KafkaTestcontainerSingleton;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import dev.vality.testcontainers.annotations.KafkaConfig;
+import dev.vality.testcontainers.annotations.kafka.KafkaTestcontainerSingleton;
+import dev.vality.testcontainers.annotations.kafka.constants.Provider;
+
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @KafkaTestcontainerSingleton(
         properties = {"kafka.topics.invoice.enabled=true"},
-        topicsKeys = {"kafka.topics.invoice.id"})
+        topicsKeys = {"kafka.topics.invoice.id"},
+        provider = Provider.APACHE)
 @KafkaConfig
 public @interface KafkaTest {
 }
